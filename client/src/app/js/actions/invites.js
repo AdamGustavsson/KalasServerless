673c92250d4b39225be95d98b3cb2e12a31257cc
Adam Gustavsson <adam.gustavsson@gmail.com>
2016-06-12 23:42:00 +0200

    Added info about the Party on the invite page.

--- client/src/app/js/actions/invites.js
+++ client/src/app/js/actions/invites.js
@@ -24,6 +24,7 @@ export function getInvite(id) {
         id,
         childName,
         inviteStatus
+        partyId
       }
     }`
   };
--- client/src/app/js/components/invites/show.jsx
+++ client/src/app/js/components/invites/show.jsx
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { getInvite, acceptInvite, rejectInvite} from '../../actions/invites';
+import { getParty } from '../../actions/parties';
 
 class InviteShow extends Component {
   componentWillMount() {
-    this.props.getInvite(this.props.params.id);
+    this.props.getInvite(this.props.params.id).then(() => this.props.getParty(this.props.invite.partyId));
+
   }
 
   onAcceptClick(event) {
@@ -17,7 +19,7 @@ class InviteShow extends Component {
 
   render() {
     const { invite } = this.props;
-
+    const { party } = this.props;
     if (!invite) {
       return <div className="row"><div className="twelve columns">Loading...</div></div>
     }
@@ -25,8 +27,12 @@ class InviteShow extends Component {
     return (
       <div className="row">
         <div className="twelve columns">
-            <p>{invite.childName}</p>
-            <p>{invite.inviteStatus}</p>
+            <p>{party.header}</p>
+            <p>Description: {party.description}</p>
+            <p>When: {party.startDateTime} - {party.endDateTime}</p>
+            <p>Where: {party.location}</p>
+            <p>Guest: {invite.childName}</p>
+            <p>Invite status: {invite.inviteStatus}</p>
             <button onClick={this.onAcceptClick.bind(this)} className="button u-full-width">Accept</button>
             <button onClick={this.onRejectClick.bind(this)} className="button u-full-width">Reject</button>
         </div>
@@ -36,7 +42,7 @@ class InviteShow extends Component {
 }
 
 function mapStateToProps(state) {
-  return { invite: state.invites.invite};
+  return { invite: state.invites.invite, party: state.parties.party};
 }
 
-export default connect(mapStateToProps, { getInvite, acceptInvite, rejectInvite })(InviteShow);
+export default connect(mapStateToProps, { getInvite, getParty, acceptInvite, rejectInvite })(InviteShow);
