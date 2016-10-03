dc720c33649b00980af699dd68636bc8f9f8b8e4
Allen Bargi <allen.bargi@gmail.com>
2016-10-03 10:16:39 +0200

    Use redux logger only in dev mode

--- client/src/app/js/store.js
+++ client/src/app/js/store.js
@@ -2,15 +2,18 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { hashHistory} from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
-
 import reducers from './reducers';
 
 const devTools = window.devToolsExtension ? window.devToolsExtension() : f => f;
+const middlewares = [thunk, routerMiddleware(hashHistory)];
+
+if (process.env.NODE_ENV === 'development') {
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger());
+}
 
 const createStoreWithMiddleware = compose(
-  applyMiddleware(thunk, createLogger()),
-  applyMiddleware(routerMiddleware(hashHistory)),
+  applyMiddleware(...middlewares),
   devTools
 )(createStore);
 
