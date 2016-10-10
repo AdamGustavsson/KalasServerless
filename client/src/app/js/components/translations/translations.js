14913cf7194e6c093c4f7faa3051ee5f35ad9490
Adam Gustavsson <adam.gustavsson@gmail.com>
2016-10-10 23:32:03 +0200

    Changed the Swedish tagline

--- client/src/app/js/components/translations/translations.js
+++ client/src/app/js/components/translations/translations.js
@@ -155,7 +155,7 @@ export const translationsObject = {
     },
     landingPage: {
       title: "Inbjudningskort till ditt kalas. Bjud in till barnkalas utan krångel | kalas.io",
-      heading: 'Barnkalas utan krångel',
+      heading: 'Kalasinbjudningar för dagens föräldrar',
       description: "Kalas.io hjälper dig att bjuda in till ditt barnkalas och hålla reda på vilka barn som kommer.",
       newPartyButton: "Skapa din första kalasinbjudan",
       howItWorks: "Hur fungerar det?",
