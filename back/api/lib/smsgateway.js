098b7339ce36c5762718d4f9bb7ab25d3426e5d5
Adam Gustavsson <adam.gustavsson@gmail.com>
2018-05-23 18:14:54 +0200

    Changed the smsgateway interface to version 4

--- back/api/lib/smsgateway.js
+++ back/api/lib/smsgateway.js
@@ -1,12 +1,17 @@
-var SmsGateway = require('smsgateway');
-
-
-const gateway = new SmsGateway(process.env.SMS_GATEWAY_USERNAME,process.env.SMS_GATEWAY_PASSWORD)
+//var SmsGateway = require('smsgateway');
+const fetch = require("node-fetch");
+const token = process.env.SMS_GATEWAY_TOKEN;
+const deviceId = process.env.SMS_GATEWAY_DEVICE_ID;
 
 function sendSMS (number, message) {
   console.log('Sending SMS to number:' + number + ' with message: ' + message);
+  var body = [{
+    "phone_number": number,
+    "message": message,
+    "device_id": deviceId
+  }]
+  return fetch('https://smsgateway.me/api/v4/message/send', { method: 'POST',headers: {'Authorization': token}, body: JSON.stringify(body) })
 
-  return gateway.send(number, message,process.env.SMS_GATEWAY_DEVICE_ID)
 }
 
 
