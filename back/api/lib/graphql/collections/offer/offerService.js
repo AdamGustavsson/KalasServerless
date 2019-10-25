4e8596ec2d8b10a3c5f6bf936132d8b7e6920975
Adam Gustavsson <adam.gustavsson@gmail.com>
2019-10-25 07:23:33 +0200

    Moved offers array to the function

--- back/api/lib/graphql/collections/offer/offerService.js
+++ back/api/lib/graphql/collections/offer/offerService.js
@@ -1,40 +1,40 @@
 'use strict';
 
-const offers =[
-  // {
-  //   id:'laserdomeMalmo',
-  //   company: 'Laserdome',
-  //   texts : {
-  //     sv: {
-  //       offerText: 'Klicka på länken igen efter %{birthdayChild}s kalas, så får du rabatt på ditt nästa %{company}-kalas.'
-  //     },
-  //     en: {
-  //       offerText: 'EN: Klicka på länken igen efter %{birthdayChild}s kalas, så får du rabatt på ditt nästa %{company}-kalas.'
-  //     }
-  //   }, 
-  //   url: 'https://boka.kalas.io/erbjudande/laserdomeMalmo',
-  //   locationRegex: /laserdome.*malm|drottning.*$/gi
-  // },
-  {
-    id: 'default',
-    company: 'PARTYKUNGEN',
-    texts : {
-      sv: {
-        offerText: 'Klicka på länken igen efter %{birthdayChild}s kalas så får du 10% rabatt på %{company} inför ditt nästa kalas.',
-        hostText: 'I samarbete med %{company} kan vi erbjuda allt du behöver inför kalaset, med 10% rabatt. %{url}'
-      },
-      en: {
-        offerText: 'Klicka på länken igen efter %{birthdayChild}s kalas så får du 10% rabatt på %{company} inför ditt nästa kalas.',
-        hostText: 'I samarbete med %{company} kan vi erbjuda allt du behöver inför kalaset, med 10% rabatt. %{url}'
-      }
-    }, 
-    url: 'https://boka.kalas.io/erbjudande/partykungen',
-    locationRegex: /.*/gi
-  }
-  
-]
 
 function identifyOffer(party){
+  const offers =[
+    // {
+    //   id:'laserdomeMalmo',
+    //   company: 'Laserdome',
+    //   texts : {
+    //     sv: {
+    //       offerText: 'Klicka på länken igen efter %{birthdayChild}s kalas, så får du rabatt på ditt nästa %{company}-kalas.'
+    //     },
+    //     en: {
+    //       offerText: 'EN: Klicka på länken igen efter %{birthdayChild}s kalas, så får du rabatt på ditt nästa %{company}-kalas.'
+    //     }
+    //   }, 
+    //   url: 'https://boka.kalas.io/erbjudande/laserdomeMalmo',
+    //   locationRegex: /laserdome.*malm|drottning.*$/gi
+    // },
+    {
+      id: 'default',
+      company: 'PARTYKUNGEN',
+      texts : {
+        sv: {
+          offerText: 'Klicka på länken igen efter %{birthdayChild}s kalas så får du 10% rabatt på %{company} inför ditt nästa kalas.',
+          hostText: 'I samarbete med %{company} kan vi erbjuda allt du behöver inför kalaset, med 10% rabatt. %{url}'
+        },
+        en: {
+          offerText: 'Klicka på länken igen efter %{birthdayChild}s kalas så får du 10% rabatt på %{company} inför ditt nästa kalas.',
+          hostText: 'I samarbete med %{company} kan vi erbjuda allt du behöver inför kalaset, med 10% rabatt. %{url}'
+        }
+      }, 
+      url: 'https://boka.kalas.io/erbjudande/partykungen',
+      locationRegex: /.*/gi
+    }
+    
+  ]
   console.log(offers);
   for (let i = 0; i < offers.length; i++) {
     const offer = offers[i];
